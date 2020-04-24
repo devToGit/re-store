568e5edea976d62d14f095a36f3e5c431b3058a1
devToGit <nnos@mail.ru>
2020-04-24 14:34:45 +0300

    1.2.3 defined and added Redux components and React-redux application frame created

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,6 +1,17 @@
 export default class BookstoreSeervice {
 
   getBooks() {
-      return [];
+      return [
+        {
+          id: 1,
+          title: 'Production-Ready Microservices',
+          author: 'Susan J Fowler'
+        },
+        {
+          id: 1,
+          title: 'Release It!',
+          author: 'Michael T. Nygard'
+        },
+      ];
   }
 }
