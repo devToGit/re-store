a0898d80a204676c4b05096dbd3cba4ba685bffe
devToGit <nnos@mail.ru>
2020-05-02 13:02:39 +0300

    1.4.0 Middleware added

--- src/store.js
+++ src/store.js
@@ -2,19 +2,19 @@ import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducers';
 
-const logMiddleware = (store) => (dispatch) => (action) => {
-    console.log(action.type, store.getState());
-    return dispatch(action);
+const logMiddleware = ({ getState, dispatch }) => (next) => (action) => {
+    console.log(action.type, getState());
+    return next(action);
 };
 
-const stringMiddleware = () => (dispatch) => (action) => {
+const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
-        return dispatch({
+        return next({
             type: action
         });
     }
 
-    return dispatch(action);
+    return next(action);
 };
 
 const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
