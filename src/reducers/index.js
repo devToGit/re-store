8475fa26de84201416a3fa439fdd4b30041ce254
devToGit <nnos@mail.ru>
2020-05-02 12:02:12 +0300

    1.3.9 Store Enhancers (string & log) added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ import updateBookList from '../reducers/book-list';
 import updateShoppingCart from '../reducers/shopping-cart';
 
 const reducer = (state, action) => {
-	console.log(action.type);
 	return {
 		bookList: updateBookList(state, action),
 		shoppingCart: updateShoppingCart(state, action)
